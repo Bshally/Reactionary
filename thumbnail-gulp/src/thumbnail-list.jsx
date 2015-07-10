81827f92f59c0f004daef6012371fded801f0af6
Bonk <brandon@usnetting.com>
2015-07-10 08:37:21 -0400

    just a bit.

--- thumbnail-gulp/src/thumbnail-list.jsx
+++ thumbnail-gulp/src/thumbnail-list.jsx
@@ -1,7 +1,10 @@
+var Thumbnail = require('thumbnail');
+
+
 var ThumbnailList = React.createClass({
-	render: function(){
-		var list = this.props.thumbnailData.map(function(thumbnailProps){
-			return <Thumbnail {...thumbnailProps} />
+  render: function(){
+    var list = this.props.thumbnailData.map(function(thumbnailProps){
+      return <Thumbnail {...thumbnailProps} />
 		});
 		return <div>
 		{list}
